9105cdbe6b07ddce9f4379e9e5a42479027aaa73
adrianindaws <adrian@indaws.es>
2022-11-20 11:51:50 +0100

    Prueba de conexion

---
+++ BackEnd/src/main/java/Erasmus/BackLA/pruebaConexion.java
@@ -0,0 +1,13 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Prueba {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nombre;
+}

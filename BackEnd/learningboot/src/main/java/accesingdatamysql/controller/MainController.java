22f37bba402694de121f2ebafffe2544dc6c2be0
adrianindaws <adrian@indaws.es>
2022-11-30 16:40:30 +0100

    sda

--- BackEnd/learningboot/src/main/java/accesingdatamysql/controller/MainController.java
+++ BackEnd/learningboot/src/main/java/accesingdatamysql/controller/MainController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import accesingdatamysql.repository.userRepository;
 
+import java.util.List;
+
 @Controller // This means that this class is a Controller
 @RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
 public class MainController {
@@ -34,4 +36,5 @@ public class MainController {
         // This returns a JSON or XML with the users
         return userRepository.findAll();
     }
+
 }
